0575d7c7d36043a7095c6d6fbf2292711d12efdc
dullat <dullat@users.noreply.github.com>
2024-05-28 21:31:20 +0530

    api module setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './scss/style.scss';
 import './scss/reset.scss';
+
+import { getWeather } from './js/api.js';
+
+getWeather('uk');
--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,32 @@ const API_KEY = '90aa7038a89840eeb84180335241405';
 const BASE_URL = 'https://api.weatherapi.com/v1/current.json';
 
 function handleError(error, message) {
-  console.error(message, error);
+  return {
+    ok: false,
+    error,
+    message,
+  };
+}
+
+function createObject(data) {
+  return {
+    name: data.location.name,
+    region: data.location.region,
+    country: data.location.country,
+    time: data.location.localtime,
+    temp_c: data.current.temp_c,
+    temp_f: data.current.temp_f,
+    isday: data.current.is_day,
+    text: data.current.condition.text,
+    icon: data.current.condition.icon,
+    wind_mph: data.current.wind_mph,
+    wind_kph: data.current.wind_kph,
+    humidity: data.current.humidity,
+    cloud: data.current.cloud,
+    feelslike_c: data.current.feelslike_c,
+    feelslike_f: data.current.feelslike_f,
+    ok: true,
+  };
 }
 
 async function getWeather(location) {
@@ -13,16 +38,14 @@ async function getWeather(location) {
 
     if (!response.ok) {
       const errorMessage = `Failed to fetch weather data for ${location}`;
-      handleError(response.statusText, errorMessage);
       throw new Error(errorMessage);
     } else {
       const data = await response.json();
-      console.log(data);
-      return data;
+      console.log(createObject(data));
+      return createObject(data);
     }
   } catch (error) {
-    handleError(error, 'Error fetching weather data');
-    throw error;
+    return handleError(error, 'Error fetching weather data');
   }
 }
 
